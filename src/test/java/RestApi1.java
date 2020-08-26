c23e43afdc3c0a3900fc48cf3cfc8056ee2f2a61
Saida Yurishi Reyes Perez <yurishama@gmail.com>
2020-08-25 23:37:55 -0500

    addTest

--- src/test/java/RestApi1.java
+++ src/test/java/RestApi1.java
@@ -7,31 +7,39 @@ import static io.restassured.RestAssured.*;
 
 public class RestApi1 {
 
+    @Test
+    public void get_token_status_fail_test(){
+        given().queryParam("lang","es").when()
+                .log().all()
+                .post("https://webapi.segundamano.mx/nga/api/v1.1/private/accounts")
+                .then().statusCode(400);
+    }
+
 
     @Test
-        public void Test_token_fail(){
-            //Request an account token without authorization header
-            RestAssured.baseURI = String.format("https://webapi.segundamano.mx/nga/api/v1.1/private/accounts");
-            Response response = given().log().all()
-                    .post();
-            //validations
-            System.out.println("Status expected: 400" );
-            System.out.println("Result: " + response.getStatusCode());
-            assertEquals(400,response.getStatusCode());
-            String errorCode = response.jsonPath().getString("error.code");
-            System.out.println("Error Code expected: VALIDATION FAILED \nResult: " + errorCode);
-            assertEquals("VALIDATION_FAILED",errorCode);
-        }
+    public void Test_token_fail() {
+        //Request an account token without authorization header
+        RestAssured.baseURI = String.format("https://webapi.segundamano.mx/nga/api/v1.1/private/accounts");
+        Response response = given().log().all()
+                .post();
+        //validations
+        System.out.println("Status expected: 400");
+        System.out.println("Result: " + response.getStatusCode());
+        assertEquals(400, response.getStatusCode());
+        String errorCode = response.jsonPath().getString("error.code");
+        System.out.println("Error Code expected: VALIDATION FAILED \nResult: " + errorCode);
+        assertEquals("VALIDATION_FAILED", errorCode);
+    }
 
-        @Test
-        public void Create_account(){
-            RestAssured.baseURI = String.format("https://webapi.segundamano.mx/nga/api/v1.1/private/accounts?lang=es");
-            Response response = given().log().all()
-                    .post();
-            System.out.println("Result: " + response.getStatusCode());
+    @Test
+    public void Create_account() {
+        RestAssured.baseURI = String.format("https://webapi.segundamano.mx/nga/api/v1.1/private/accounts?lang=es");
+        Response response = given().log().all()
+                .post();
+        System.out.println("Result: " + response.getStatusCode());
 
 
-        }
+    }
 
     @Test
     public void validateStatusCode() {
@@ -40,12 +48,6 @@ public class RestApi1 {
                 .then().statusCode(401);
     }
 
-    @Test
-    public void validateStatusCode_extract_response() {
-        Response response = given().queryParam("lang", "es").when()
-                .get("https://webapi.segundamano.mx/nga/api/v1.1/private/accounts")
-                .then().extract().response().statusCode(401);
-        System.out.println ("Response: " + response.asString ());
 
 
 }
